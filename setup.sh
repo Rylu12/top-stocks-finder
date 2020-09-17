bb5183f7995676935906eb2feeac3b391aedab62
Rylu12 <52330721+Rylu12@users.noreply.github.com>
2020-09-16 18:04:40 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 mkdir -p ~/.streamlit/
-
-echo "\
+echo “\
+[general]\n\
+email = \”your-email@domain.com\”\n\
+“ > ~/.streamlit/credentials.toml
+echo “\
 [server]\n\
-port = $PORT\n\
-enableCORS = false\n\
 headless = true\n\
-\n\
-"> ~/.streamlit/config.toml
+enableCORS=false\n\
+port = $PORT\n\
+“ > ~/.streamlit/config.toml
